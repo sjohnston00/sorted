4d2848f4d8f1aca15ec623e2e96c6e282419b50d
Sam Johnston <88184024+sjohnston-jarrold@users.noreply.github.com>
2022-03-24 11:19:48 +0000

    Redirected home route to dashboard - Fixing error messages on register page

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,6 @@
 import { useState } from "react"
-import { ActionFunction, Form, Link } from "remix"
+import { Link, LoaderFunction, redirect } from "remix"
 
-export const action: ActionFunction = async ({ request }) => {
-  const formData = await request.formData()
-  const _id = formData.get("_id")
-  const habitName = formData.get("name")
-  const colour = formData.get("colour")
-
-  return {
-    _id,
-    habitName,
-    colour,
-  }
-}
-
-export default function Index() {
-  return (
-    <main className="container mx-2">
-      <h1 className="text-2xl">
-        Welcome to <span className="text-3xl italic">"Sorted"</span>
-      </h1>
-      <Link
-        to={"/habits"}
-        className="text-blue-400 hover:text-blue-500 hover:underline"
-      >
-        My Habits
-      </Link>
-      <br />
-    </main>
-  )
+export const loader: LoaderFunction = (): Response => {
+  return redirect("/dashboard")
 }
--- app/routes/register.tsx
+++ app/routes/register.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from "react"
 import {
   ActionFunction,
   Form,
@@ -9,132 +9,134 @@ import {
   LoaderFunction,
   redirect,
   Link,
-  MetaFunction
-} from "remix";
+  MetaFunction,
+} from "remix"
 import {
   isValidPassword,
   registerUser,
-  usernameExists
-} from "~/utils/register.server";
+  usernameExists,
+} from "~/utils/register.server"
 import {
   createUserSession,
   getUserId,
   loginUser,
-  requireUserId
-} from "~/utils/session.server";
+  requireUserId,
+} from "~/utils/session.server"
 
 export const meta: MetaFunction = () => {
   return {
-    title: "Sorted | Register"
-  };
-};
+    title: "Sorted | Register",
+  }
+}
 
 export const loader: LoaderFunction = async ({ request }) => {
-  const userId = await getUserId(request);
+  const userId = await getUserId(request)
   if (userId) {
-    return redirect("/habits");
+    return redirect("/habits")
   }
-  return null;
-};
+  return null
+}
 
 export const action: ActionFunction = async ({ request }) => {
-  const formData = await request.formData();
-  const username = formData.get("username")?.toString();
-  const password = formData.get("password")?.toString();
-  const redirectTo = formData.get("redirectTo")?.toString() || "/habits";
+  const formData = await request.formData()
+  const username = formData.get("username")?.toString()
+  const password = formData.get("password")?.toString()
+  const redirectTo = formData.get("redirectTo")?.toString() || "/habits"
 
   if (!username || !password || !redirectTo) {
     return {
       errors: {
         username: "Please provide a username",
-        password: "Please provide a password"
-      }
-    };
+        password: "Please provide a password",
+      },
+    }
   }
 
-  const validPassword = isValidPassword(password);
+  const validPassword = isValidPassword(password)
   if (!validPassword) {
     return {
       errors: {
         password:
-          "Please provide a password that is at least 8 characters long, 1 uppercase, 1 number and 1 special character"
-      }
-    };
+          "Please provide a password that is at least 8 characters long, 1 uppercase, 1 number and 1 special character",
+      },
+    }
   }
 
-  const usernameTaken = await usernameExists(username);
+  const usernameTaken = await usernameExists(username)
   if (usernameTaken) {
     return {
       errors: {
-        username: "This username has already been taken, please try another one"
-      }
-    };
+        username:
+          "This username has already been taken, please try another one",
+      },
+    }
   }
 
-  const user = await registerUser(username, password);
+  const user = await registerUser(username, password)
 
-  return createUserSession(user._id, redirectTo);
-};
+  return createUserSession(user._id, redirectTo)
+}
 
 export default function Register() {
-  const [searchParams] = useSearchParams();
-  const actionData = useActionData();
-  const transition = useTransition();
+  const [searchParams] = useSearchParams()
+  const actionData = useActionData()
+  const transition = useTransition()
 
-  const submitting = transition.state === "submitting";
+  const submitting = transition.state === "submitting"
   return (
     <>
-      <h1 className='text-4xl'>Register</h1>
-      <Form method='post' className='auth-form'>
-        <label htmlFor='username'>
-          Username <span className='text-red-600'>*</span>
+      <h1 className="text-4xl">Register</h1>
+      <Form method="post" className="auth-form">
+        <label htmlFor="username">
+          Username <span className="text-red-600">*</span>
         </label>
         <input
-          type='text'
-          name='username'
-          id='username'
-          autoComplete='username'
+          type="text"
+          name="username"
+          id="username"
+          autoComplete="username"
           minLength={3}
           maxLength={20}
-          className='input'
+          className="input"
           required
         />
-        <small className='text-red-600'>
+        <small className="block py-2 text-red-600">
           {actionData?.errors?.username}&nbsp;
         </small>
-        <label htmlFor='password'>
-          Password <span className='text-red-600'>*</span>
+        <label htmlFor="password">
+          Password <span className="text-red-600">*</span>
         </label>
         <input
-          type='password'
-          name='password'
-          id='password'
-          autoComplete='password'
+          type="password"
+          name="password"
+          id="password"
+          autoComplete="password"
           minLength={6}
           maxLength={64}
-          className='input'
+          className="input"
           required
         />
-        <small className='text-red-600'>
+        <small className="block py-2 text-red-600">
           {actionData?.errors?.password}&nbsp;
         </small>
         <input
-          type='hidden'
-          name='redirectTo'
+          type="hidden"
+          name="redirectTo"
           value={searchParams.get("redirectTo") ?? undefined}
         />
-        <div className='flex items-center gap-2'>
+        <div className="flex items-center gap-2">
           <button
-            type='submit'
-            className='btn btn-primary mt-2  mr-2'
-            disabled={submitting}>
+            type="submit"
+            className="btn btn-primary mt-2  mr-2"
+            disabled={submitting}
+          >
             {submitting ? "Registering... " : "Register"}
           </button>
-          <Link to={"/login"} className='link'>
+          <Link to={"/login"} className="link">
             Already have an account? Login
           </Link>
         </div>
       </Form>
     </>
-  );
+  )
 }
