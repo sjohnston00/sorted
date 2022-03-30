bd5a55adf13bf30ae1ea26a66056f07740af64b9
Sam Johnston <88184024+sjohnston-jarrold@users.noreply.github.com>
2022-03-30 09:24:19 +0100

    Redirecting from home page to dashboard

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,10 +2,10 @@ import { useState } from "react"
 import { Link, LoaderFunction, redirect } from "remix"
 import CustomCalendar from "~/components/customCalendar"
 
-// export const loader: LoaderFunction = (): Response => {
-//   return redirect("/dashboard")
-// }
-
-export default function Index() {
-  return <div></div>
+export const loader: LoaderFunction = (): Response => {
+  return redirect("/dashboard")
 }
+
+// export default function Index() {
+//   return <div></div>
+// }
