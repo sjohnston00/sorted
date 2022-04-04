14b7f7bfbdd4b64f3de517d9e0089157bf2978fc
Sam Jonston <johnstonsam712@gmail.com>
2022-04-04 17:29:57 +0100

    Fixing `/habits` display colours

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import {
   ActionFunction,
   Form,
@@ -14,132 +14,132 @@ import {
   useLocation,
   useNavigate,
   useOutlet,
-  useTransition,
-} from "remix"
-import CalendarComponent from "~/components/calendar"
-import { requireUserId } from "~/utils/session.server"
-import { getHabitsForUser } from "~/utils/habits.server"
-import { getMarkedHabitsForUser } from "~/utils/markedHabits.server"
-import useIsMount from "~/utils/hooks/useIsMount"
+  useTransition
+} from "remix";
+import CalendarComponent from "~/components/calendar";
+import { requireUserId } from "~/utils/session.server";
+import { getHabitsForUser } from "~/utils/habits.server";
+import { getMarkedHabitsForUser } from "~/utils/markedHabits.server";
+import useIsMount from "~/utils/hooks/useIsMount";
 import {
   MarkedHabitWithHabit,
-  MarkedHabitWithId,
-} from "~/types/markedHabit.server"
-import CustomCalendar from "~/components/customCalendar"
-import { AnimatePresence, motion, Variants } from "framer-motion"
-import Modal from "~/components/modal"
-import LoadingIndicator from "~/components/LoadingIndicator"
-import { HiOutlineX, HiPlus } from "react-icons/hi"
-import { Habit, HabitWithId } from "~/types/habits.server"
-import MarkedHabit from "~/models/MarkedHabit.server"
-import mongoose from "mongoose"
+  MarkedHabitWithId
+} from "~/types/markedHabit.server";
+import CustomCalendar from "~/components/customCalendar";
+import { AnimatePresence, motion, Variants } from "framer-motion";
+import Modal from "~/components/modal";
+import LoadingIndicator from "~/components/LoadingIndicator";
+import { HiOutlineX, HiPlus } from "react-icons/hi";
+import { Habit, HabitWithId } from "~/types/habits.server";
+import MarkedHabit from "~/models/MarkedHabit.server";
+import mongoose from "mongoose";
 
 type LoaderData = {
-  markedHabits: Array<MarkedHabitWithHabit>
-  habits: Array<HabitWithId>
-}
+  markedHabits: Array<MarkedHabitWithHabit>;
+  habits: Array<HabitWithId>;
+};
 
 type ActionData = {
   errors?: {
-    message?: string
-    habit?: string
-    markedDate?: string
-  }
-}
+    message?: string;
+    habit?: string;
+    markedDate?: string;
+  };
+};
 
 type FetcherData = {
-  habits: Array<HabitWithId>
-  markedHabits: Array<MarkedHabitWithHabit>
-}
+  habits: Array<HabitWithId>;
+  markedHabits: Array<MarkedHabitWithHabit>;
+};
 
 export const meta: MetaFunction = () => {
   return {
-    title: `Sorted | Dashboard`,
-  }
-}
+    title: `Sorted | Dashboard`
+  };
+};
 
 export const loader: LoaderFunction = async ({
-  request,
+  request
 }): Promise<LoaderData> => {
-  const userId = await requireUserId(request)
+  const userId = await requireUserId(request);
   const markedHabits = await getMarkedHabitsForUser(
     userId,
     new Date(0),
     new Date(2100, 0)
-  )
-  const habits = await getHabitsForUser(userId)
+  );
+  const habits = await getHabitsForUser(userId);
 
   return {
     markedHabits: markedHabits,
-    habits: habits,
-  }
-}
+    habits: habits
+  };
+};
 
 export const action: ActionFunction = async ({ request }) => {
-  const userId = await requireUserId(request)
-  const formData = await request.formData()
+  const userId = await requireUserId(request);
+  const formData = await request.formData();
 
   if (request.method === "DELETE") {
-    const markedHabitId = formData.get("markedHabitId")
-    console.log(markedHabitId)
-    console.log(userId)
+    const markedHabitId = formData.get("markedHabitId");
+    console.log(markedHabitId);
+    console.log(userId);
     if (typeof markedHabitId !== "string") {
-      return {}
+      return {};
     }
     await MarkedHabit.deleteOne({
       _id: markedHabitId,
-      user: userId,
-    })
-    return {}
+      user: userId
+    });
+    return {};
   }
-  const habitId = formData.get("selectedHabit")
-  const selectedDate = formData.get("selectedDate")
+  const habitId = formData.get("selectedHabit");
+  const selectedDate = formData.get("selectedDate");
   if (typeof habitId !== "string" || typeof selectedDate !== "string") {
     return {
       errors: {
         message: "Please provide a valid Habit",
-        markedDate: "Please provide a valid Date",
-      },
-    }
+        markedDate: "Please provide a valid Date"
+      }
+    };
   }
 
   const newMarkedHabit = new MarkedHabit({
     user: new mongoose.Types.ObjectId(userId),
     habit: new mongoose.Types.ObjectId(habitId),
-    date: new Date(selectedDate),
-  })
+    date: new Date(selectedDate)
+  });
 
-  await newMarkedHabit.save()
-  return {}
-}
+  await newMarkedHabit.save();
+  return {};
+};
 
 export default function Dashboard() {
-  const { markedHabits, habits } = useLoaderData<LoaderData>()
-  const actionData = useActionData<ActionData>()
-  const [modalOpen, setModalOpen] = useState(false)
-  const isMount = useIsMount()
-  const [selectedDate, setSelectedDate] = useState(new Date())
+  const { markedHabits, habits } = useLoaderData<LoaderData>();
+  const actionData = useActionData<ActionData>();
+  const [modalOpen, setModalOpen] = useState(false);
+  const isMount = useIsMount();
+  const [selectedDate, setSelectedDate] = useState(new Date());
   const [selectedDateMarkedHabits, setSelectedDateMarkedHabits] = useState<
     Array<MarkedHabitWithHabit>
-  >([])
-  const transition = useTransition()
-  const transitionIsSubmitting = transition.submission?.method === "POST"
+  >([]);
+  const transition = useTransition();
+  const transitionIsSubmitting = transition.submission?.method === "POST";
   const handleClose = () => {
-    setModalOpen(false)
-  }
+    setModalOpen(false);
+  };
 
   useEffect(() => {
-    if (isMount) return
-    setModalOpen(true)
+    if (isMount) return;
+    setModalOpen(true);
     const nextDate = new Date(
       new Date(selectedDate).setDate(selectedDate.getDate() + 1)
-    )
+    );
     const selectedMarkedHabits = markedHabits.filter((markedHabit) => {
-      const markedHabitDate = new Date(markedHabit.date)
-      return markedHabitDate >= selectedDate && markedHabitDate < nextDate
-    })
-    setSelectedDateMarkedHabits(selectedMarkedHabits)
-  }, [selectedDate, markedHabits])
+      const markedHabitDate = new Date(markedHabit.date);
+      return markedHabitDate >= selectedDate && markedHabitDate < nextDate;
+    });
+    setSelectedDateMarkedHabits(selectedMarkedHabits);
+  }, [selectedDate, markedHabits]);
 
   return (
     <>
@@ -152,38 +152,35 @@ export default function Dashboard() {
       <AnimatePresence
         exitBeforeEnter
         initial={false}
-        onExitComplete={() => null}
-      >
+        onExitComplete={() => null}>
         {modalOpen && (
           <Modal handleClose={handleClose}>
-            <div className="modal-body">
-              <div className="flex justify-between items-center">
-                <h2 className="text-2xl">{selectedDate.toDateString()}</h2>
+            <div className='modal-body'>
+              <div className='flex justify-between items-center'>
+                <h2 className='text-2xl'>{selectedDate.toDateString()}</h2>
                 <button
-                  className="p-2 rounded-sm hover:scale-110 hover:opacity-100 opacity-70 transition-all text-xl"
+                  className='p-2 rounded-sm hover:scale-110 hover:opacity-100 opacity-70 transition-all text-xl'
                   onClick={handleClose}
-                  title="Close"
-                >
+                  title='Close'>
                   &times;
                 </button>
               </div>
               {habits.length || 0 > 0 ? (
                 <>
-                  <div className="flex flex-col gap-2">
+                  <div className='flex flex-col gap-2'>
                     {selectedDateMarkedHabits.map((markedHabit) => (
                       <ModalMarkedHabit markedHabit={markedHabit} />
                     ))}
                   </div>
-                  <Form method="post">
-                    <small className="block text-danger p-2">
+                  <Form method='post'>
+                    <small className='block text-danger p-2'>
                       {actionData && actionData.errors?.message}&nbsp;
                     </small>
                     <select
-                      name="selectedHabit"
-                      id="selectedHabit"
-                      className="block mb-2 bg-neutral-600 p-2 rounded-sm cursor-pointer text-neutral-50"
-                      required
-                    >
+                      name='selectedHabit'
+                      id='selectedHabit'
+                      className='block mb-2 bg-neutral-600 p-2 rounded-sm cursor-pointer text-neutral-50'
+                      required>
                       <option defaultValue={undefined} disabled hidden>
                         Select a habit
                       </option>
@@ -196,31 +193,29 @@ export default function Dashboard() {
 
                     <input
                       type={"hidden"}
-                      name="selectedDate"
-                      id="selectedDate"
+                      name='selectedDate'
+                      id='selectedDate'
                       value={selectedDate.toISOString()}
                     />
                     <button
-                      className="btn flex gap-1 text-neutral-50 transition-all bg-neutral-600 hover:bg-neutral-700 focus:bg-neutral-700"
-                      type="submit"
-                      disabled={transitionIsSubmitting}
-                    >
+                      className='btn flex gap-1 text-neutral-50 transition-all bg-neutral-600 hover:bg-neutral-700 focus:bg-neutral-700'
+                      type='submit'
+                      disabled={transitionIsSubmitting}>
                       Mark
                       {transitionIsSubmitting ? (
-                        <LoadingIndicator className="spinner static h-6 w-6" />
+                        <LoadingIndicator className='spinner static h-6 w-6' />
                       ) : null}
                     </button>
                   </Form>
                 </>
               ) : (
                 <>
-                  <p className="py-2 text-red-500">
+                  <p className='py-2 text-red-500'>
                     You don't have any habits to add.{" "}
                   </p>
                   <Link
                     to={"/habits/new"}
-                    className="btn btn-primary inline-flex gap-2 items-center"
-                  >
+                    className='btn btn-primary inline-flex gap-2 items-center'>
                     Create One <HiPlus />
                   </Link>
                 </>
@@ -229,52 +224,45 @@ export default function Dashboard() {
           </Modal>
         )}
       </AnimatePresence>
-      {/* </div>
-      <button onClick={() => onChange(new Date())} className="btn btn-primary">
-        Today
-      </button> */}
     </>
-  )
+  );
 }
 function ModalMarkedHabit({
-  markedHabit,
+  markedHabit
 }: {
-  markedHabit: MarkedHabitWithHabit
+  markedHabit: MarkedHabitWithHabit;
 }) {
-  const fetcher = useFetcher()
+  const fetcher = useFetcher();
   return (
     <div
       className={`flex gap-2 items-center ${
         fetcher.submission?.formData.get("markedHabitId") === markedHabit._id
           ? "opacity-30"
           : ""
-      } transition-all`}
-    >
+      } transition-all`}>
       <Link
-        className="flex items-center text-reset"
+        className='flex items-center text-reset'
         key={markedHabit.habit._id}
         to={`/habits/${markedHabit.habit._id}`}
-        title={`${markedHabit.habit.name} - Colour: ${markedHabit.habit.colour}`}
-      >
+        title={`${markedHabit.habit.name} - Colour: ${markedHabit.habit.colour}`}>
         {markedHabit.habit.name}
         <div
-          className="h-6 w-6 ml-2 inline-block shadow-sm rounded-full align-middle"
+          className='h-6 w-6 ml-2 inline-block shadow-sm rounded-full align-middle'
           style={{
-            backgroundColor: markedHabit.habit.colour,
-          }}
-        ></div>
+            backgroundColor: markedHabit.habit.colour
+          }}></div>
       </Link>
-      <fetcher.Form method="delete" className="flex items-center">
+      <fetcher.Form method='delete' className='flex items-center'>
         <input
           type={"hidden"}
-          name="markedHabitId"
-          id="markedHabitId"
+          name='markedHabitId'
+          id='markedHabitId'
           value={markedHabit._id}
         />
-        <button type="submit" className="p-1">
-          <HiOutlineX className="hover:scale-125 transition-all opacity-70 hover:opacity-100" />
+        <button type='submit' className='p-1'>
+          <HiOutlineX className='hover:scale-125 transition-all opacity-70 hover:opacity-100' />
         </button>
       </fetcher.Form>
     </div>
-  )
+  );
 }
--- app/routes/habits.tsx
+++ app/routes/habits.tsx
@@ -1,51 +1,50 @@
-import React from "react"
-import Habit from "~/models/Habit.server"
+import React from "react";
+import Habit from "~/models/Habit.server";
 import {
   Link,
   LoaderFunction,
   MetaFunction,
   Outlet,
   redirect,
-  useLoaderData,
-} from "remix"
-import { requireUserId } from "~/utils/session.server"
+  useLoaderData
+} from "remix";
+import { requireUserId } from "~/utils/session.server";
 
 export const meta: MetaFunction = () => {
   return {
-    title: "Sorted | Habits",
-  }
-}
+    title: "Sorted | Habits"
+  };
+};
 
 export const loader: LoaderFunction = async ({ request }) => {
-  const userId = await requireUserId(request)
-  const habits = await Habit.find({ user: userId }).populate("user")
-  return habits
-}
+  const userId = await requireUserId(request);
+  const habits = await Habit.find({ user: userId }).populate("user");
+  return habits;
+};
 
 export default function Index() {
-  const data = useLoaderData()
+  const data = useLoaderData();
   return (
     <>
-      <h1 className="text-3xl">Habits</h1>
+      <h1 className='text-3xl'>Habits</h1>
       <ul>
         {data.length > 0 ? (
           data.map((habit: any) => (
-            <li key={habit._id} className="hover:underline cursor-pointer mb-2">
+            <li key={habit._id} className='hover:underline cursor-pointer mb-2'>
               <Link to={habit._id} title={`${habit.name}`}>
                 {habit.name}
               </Link>
               <div
-                className="h-6 w-6 ml-2 inline-block border-2 border-slate-900 align-middle"
+                className='h-6 w-6 ml-2 inline-block shadow-sm rounded-full align-middle'
                 style={{ backgroundColor: habit.colour }}
-                title={`Colour: ${habit.colour}`}
-              ></div>
+                title={`Colour: ${habit.colour}`}></div>
             </li>
           ))
         ) : (
-          <p className="py-2 text-red-500">You don't have any habits.</p>
+          <p className='py-2 text-red-500'>You don't have any habits.</p>
         )}
       </ul>
       <Outlet />
     </>
-  )
+  );
 }
