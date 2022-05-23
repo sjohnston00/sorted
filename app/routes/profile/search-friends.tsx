4046f95356a58d267960db7534c7981ba5d36c4b
Sam Johnston <88184024+sjohnston-jarrold@users.noreply.github.com>
2022-05-23 09:54:34 +0100

    Showing friend requests when searching users

--- app/routes/profile/search-friends.tsx
+++ app/routes/profile/search-friends.tsx
@@ -7,19 +7,35 @@ import {
   useFetcher,
   useLoaderData,
   useMatches,
+  useSearchParams,
 } from "remix"
 import User from "~/models/User.server"
 import type { User as UserType } from "~/types/user.server"
-import { HiCheck, HiPlus } from "react-icons/hi"
+import {
+  HiCheck,
+  HiOutlineRefresh,
+  HiOutlineUser,
+  HiOutlineUserAdd,
+  HiOutlineUserRemove,
+  HiPlus,
+  HiUser,
+  HiX,
+} from "react-icons/hi"
 import type { Document, Types } from "mongoose"
 import { getUser, getUserId, requireUserId } from "~/utils/session.server"
 import { getUserDetails } from "~/utils/user.server"
 import { createFriendRequest } from "~/utils/friends.server"
 import FriendRequest from "~/models/FriendRequest.server"
+import { FriendRequest as FriendRequestType } from "~/types/friends.server"
 
 type LoaderData = {
   users?: Array<
     UserType & {
+      _id: string
+    }
+  >
+  myFriendRequests: Array<
+    FriendRequestType & {
       _id: string
     }
   >
@@ -38,10 +54,15 @@ export const loader: LoaderFunction = async ({ request }) => {
         username: new RegExp(searchedUsername, "i"),
         visibility: "public",
       })
+      const myFriendRequests = await FriendRequest.find({
+        $or: [{ from: userId }, { to: userId }],
+        accepted: false,
+      })
 
       //TODO: sort the users by if they are friends first
       return {
         users: users.filter((user) => !user._id.equals(userId)),
+        myFriendRequests,
       }
     } else {
       return {}
@@ -54,27 +75,67 @@ export const action: ActionFunction = async ({ request }) => {
   const userId = await requireUserId(request)
   const formData = await request.formData()
   const data = Object.fromEntries(formData)
-
-  if (data._action === "add-friend") {
-    //TODO: Validate that the user is not already friends with the user
-    //TODO: Validate that the user has not already sent a friend request
-
+  //TODO: Get the friend request ID
+  //TODO: Validate that the user is not already friends with the user
+  //TODO: Validate that the user has not already sent a friend request
+  if (data._action === "send-friend-request") {
     const friendRequest = await createFriendRequest(
       userId,
       data.userId.toString()
     )
     return { friendRequest }
   }
-  await new Promise((resolve) => setTimeout(resolve, 1500))
+
+  if (data._action === "cancel-friend-request") {
+    const friendRequest = await FriendRequest.updateOne(
+      { from: userId, to: data.userId },
+      { $set: { accepted: true } }
+    )
+    return { friendRequest }
+  }
+  if (data._action === "accept-friend-request") {
+    const friendRequest = await FriendRequest.updateOne(
+      { from: userId, to: data.userId },
+      { $set: { accepted: true } }
+    )
+    await User.updateOne(
+      { _id: userId },
+      {
+        $push: { friends: data.userId },
+      }
+    )
+    await User.updateOne(
+      { _id: data.userId },
+      {
+        $push: { friends: userId },
+      }
+    )
+    return { friendRequest }
+  }
+  if (data._action === "decline-friend-request") {
+    const friendRequest = await FriendRequest.updateOne(
+      { from: userId, to: data.userId },
+      { $set: { accepted: true } }
+    )
+    return { friendRequest }
+  }
+
   return { data }
 }
 
 export default function SearchFriends() {
   const data = useLoaderData<LoaderData>()
+  const [searchParams] = useSearchParams()
   return (
     <div>
       <Form method="get">
-        <input type="text" className="input" name="username" id="username" />
+        <input
+          type="text"
+          className="input"
+          name="username"
+          id="username"
+          defaultValue={searchParams.get("username") || ""}
+        />
         <div className="flex gap-2 py-2">
           <Link className="btn btn-dark" to={"/profile"}>
             Back
@@ -86,7 +147,11 @@ export default function SearchFriends() {
       </Form>
       <div className="flex flex-col gap-4 p-2">
         {data?.users?.map((user) => (
-          <UserRow user={user} key={user._id} />
+          <UserRow
+            user={user}
+            key={user._id}
+            myFriendRequests={data.myFriendRequests}
+          />
         ))}
       </div>
     </div>
@@ -95,13 +160,29 @@ export default function SearchFriends() {
 
 type UserRowProps = {
   user: UserType & { _id: string }
+  myFriendRequests: Array<
+    FriendRequestType & {
+      _id: string
+    }
+  >
 }
 
-function UserRow({ user }: UserRowProps) {
+function UserRow({ user, myFriendRequests }: UserRowProps) {
   const [rootLoader] = useMatches()
   const loggedInUser = rootLoader.data.user
   const fetcher = useFetcher()
   const isFriend = loggedInUser?.friends?.includes(user._id)
+
+  const myFriendRequestPending = myFriendRequests.some(
+    (friendRequest) =>
+      friendRequest.from === loggedInUser._id &&
+      friendRequest.to === (user._id as any)
+  )
+  const incomingFriendRequestPending = myFriendRequests.some(
+    (friendRequest) =>
+      friendRequest.from === (user._id as any) &&
+      friendRequest.to === loggedInUser._id
+  )
 
   return (
     <div
@@ -124,15 +205,52 @@ function UserRow({ user }: UserRowProps) {
       </div>
       <fetcher.Form method="post">
         <input type="hidden" name="userId" id="userId" value={user._id} />
-        <input type="hidden" name="_action" id="_action" value="add-friend" />
-
-        <button
-          style={{ opacity: fetcher.state === "submitting" ? 0.5 : 1 }}
-          className={`transition-all p-2 `}
-          disabled={isFriend}
-        >
-          {isFriend ? <HiCheck /> : <HiPlus />}
-        </button>
+        {incomingFriendRequestPending ? (
+          <>
+            <button
+              className={`p-2`}
+              name="_action"
+              id="_action"
+              title="Accept Friend Request"
+              value="accept-friend-request"
+            >
+              <HiOutlineUserAdd />
+            </button>
+            <button
+              className={`p-2`}
+              name="_action"
+              id="_action"
+              title="Decline Friend Request"
+              value="decline-friend-request"
+            >
+              <HiOutlineUserRemove />
+            </button>
+          </>
+        ) : myFriendRequestPending ? (
+          <div className="flex items-center">
+            <HiUser className={`h-8 w-8`} title="Friend Request Pending" />
+            <button
+              className={`p-2`}
+              name="_action"
+              id="_action"
+              title="Cancel Friend Request"
+              value="cancel-friend-request"
+            >
+              <HiX />
+            </button>
+          </div>
+        ) : (
+          <button
+            className={`p-2`}
+            name="_action"
+            id="_action"
+            title="Send Friend Request"
+            value="send-friend-request"
+            disabled={isFriend}
+          >
+            {!isFriend && <HiOutlineUserAdd />}
+          </button>
+        )}
       </fetcher.Form>
     </div>
   )
