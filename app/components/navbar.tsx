c8d40cb26746bf78c799592e5639d9af813f4cbd
Sam Jonston <johnstonsam712@gmail.com>
2022-06-16 19:47:21 +0100

    removing home link from navbar

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -14,29 +14,7 @@ import {
 export default function Navbar() {
   return (
     <nav className="nav translucent-blur">
-      <ul className="flex items-center lg:justify-evenly md:justify-around justify-between  gap-2 w-full">
-        <li>
-          <NavLink to="home" title="Home" className="hover:no-underline">
-            {({ isActive }) =>
-              isActive ? (
-                <div className="flex flex-col items-center">
-                  <HiHome size={"1.25em"} />
-                  <span className="text-xs font-semibold">Home</span>
-                </div>
-              ) : (
-                <div className="flex flex-col items-center opacity-80">
-                  <HiOutlineHome
-                    size={"1.25em"}
-                    className="dark:stroke-neutral-50 stroke-neutral-800"
-                  />
-                  <span className="dark:text-neutral-50 text-neutral-800 text-xs">
-                    Home
-                  </span>
-                </div>
-              )
-            }
-          </NavLink>
-        </li>
+      <ul className="flex items-center justify-around  gap-2 w-full">
         <li>
           <NavLink to="habits" title="Habits" className="hover:no-underline">
             {({ isActive }) =>
