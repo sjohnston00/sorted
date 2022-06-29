9ac4c0a34f64ce1cc658d9bbec6b7ab4e44ca60a
Sam Jonston <johnstonsam712@gmail.com>
2022-06-29 20:45:04 +0100

    Listing habits on non layout route

--- app/routes/habits.tsx
+++ app/routes/habits.tsx
@@ -1,16 +1,5 @@
 import React from "react"
-import { default as DBHabit } from "~/models/Habit.server"
-import HabitBox from "~/components/HabitBox"
-import {
-  Link,
-  LoaderFunction,
-  MetaFunction,
-  Outlet,
-  useLoaderData,
-} from "remix"
-import { requireUserId } from "~/utils/session.server"
-import { MongoDocument } from "~/types"
-import { Habit as HabitType } from "~/types/habits.server"
+import { MetaFunction, Outlet } from "remix"
 
 export const meta: MetaFunction = () => {
   return {
@@ -18,24 +7,9 @@ export const meta: MetaFunction = () => {
   }
 }
 
-export const loader: LoaderFunction = async ({ request }) => {
-  const userId = await requireUserId(request)
-  const habits = await DBHabit.find({ user: userId }).populate("user")
-  return habits
-}
-
 export default function Index() {
-  const data = useLoaderData<MongoDocument<HabitType>[]>()
   return (
     <>
-      <h1 className="text-3xl">Habits</h1>
-      <ul className="flex flex-wrap py-4 gap-10 justify-center">
-        {data.length > 0 ? (
-          data.map((habit) => <HabitBox key={habit._id} habit={habit} />)
-        ) : (
-          <p className="py-2 text-red-500">You do not have any habits.</p>
-        )}
-      </ul>
       <Outlet />
     </>
   )
--- app/routes/habits/index.tsx
+++ app/routes/habits/index.tsx
@@ -1,10 +1,33 @@
 import React from "react"
-import { Link } from "remix"
+import { Link, LoaderFunction, useLoaderData } from "remix"
+import { requireUserId } from "~/utils/session.server"
+import { default as DBHabit } from "~/models/Habit.server"
+import { Habit as HabitType } from "~/types/habits.server"
+import { MongoDocument } from "~/types"
+import HabitBox from "~/components/HabitBox"
+
+export const loader: LoaderFunction = async ({ request }) => {
+  const userId = await requireUserId(request)
+  const habits = await DBHabit.find({ user: userId }).populate("user")
+  return habits
+}
 
 export default function Index() {
+  const data = useLoaderData<MongoDocument<HabitType>[]>()
+
   return (
-    <Link to={"./new"} className="btn btn-primary">
-      New
-    </Link>
+    <>
+      <h1 className="text-3xl">Habits</h1>
+      <ul className="flex flex-wrap py-4 gap-10 justify-center">
+        {data.length > 0 ? (
+          data.map((habit) => <HabitBox key={habit._id} habit={habit} />)
+        ) : (
+          <p className="py-2 text-red-500">You do not have any habits.</p>
+        )}
+      </ul>
+      <Link to={"./new"} className="btn btn-primary">
+        New
+      </Link>
+    </>
   )
 }
