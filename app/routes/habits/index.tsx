adeb7e6e06056883696002edb8fa94a99c7e4109
Sam Jonston <johnstonsam712@gmail.com>
2022-07-05 14:25:16 +0100

    Styling new habit button

--- app/routes/habits/index.tsx
+++ app/routes/habits/index.tsx
@@ -5,6 +5,7 @@ import { default as DBHabit } from "~/models/Habit.server"
 import { Habit as HabitType } from "~/types/habits.server"
 import { MongoDocument } from "~/types"
 import HabitBox from "~/components/HabitBox"
+import { HiPlus } from "react-icons/hi"
 
 type LoaderData = {
   habits: MongoDocument<HabitType>[]
@@ -26,16 +27,27 @@ export default function Index() {
       </h1>
       <ul className="flex flex-wrap py-4 gap-10">
         {habits.length > 0 ? (
-          habits.map((habit) => <HabitBox key={habit._id} habit={habit} />)
+          habits.map((habit) => (
+            <li key={habit._id}>
+              <HabitBox habit={habit} />
+            </li>
+          ))
         ) : (
           <p className="text-neutral-400 text-sm font-light">
             You are currently not tracking any habits
           </p>
         )}
+
+        <li>
+          <Link
+            className="p-1 h-40 w-40 bg-opacity-20 border-4 border-solid rounded-lg flex items-center justify-center font-bold tracking-wide bg-neutral-50 text-neutral-50 hover:text-neutral-50"
+            title="Create a new habit"
+            to={"new"}
+          >
+            <HiPlus />
+          </Link>
+        </li>
       </ul>
-      <Link to={"new"} className="btn btn-primary">
-        New
-      </Link>
     </>
   )
 }
