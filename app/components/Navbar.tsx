76803f32d760954593973c5f5bcfc6a941207914
Sam Johnston <johnstonsam712@gmail.com>
2023-06-23 14:28:51 +0100

    add min navbar height

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,14 +1,13 @@
-import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/remix'
-import React from 'react'
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/remix"
 
 export default function Navbar() {
   return (
-    <nav className='flex p-4 items-center justify-end'>
+    <nav className="flex p-4 items-center justify-end min-h-[4rem]">
       <SignedIn>
         <UserButton showName />
       </SignedIn>
       <SignedOut>
-        <SignInButton mode='redirect' redirectUrl='/' />
+        <SignInButton mode="redirect" redirectUrl="/" />
       </SignedOut>
     </nav>
   )
