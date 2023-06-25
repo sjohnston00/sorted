5e16b0e46e6569bd7a97556ddcf37f6da464f845
Sam Johnston <johnstonsam712@gmail.com>
2023-06-25 15:47:32 +0100

    add PWA support

--- app/root.tsx
+++ app/root.tsx
@@ -13,8 +13,18 @@ import { rootAuthLoader } from '@clerk/remix/ssr.server'
 import { ClerkApp, ClerkCatchBoundary } from '@clerk/remix/'
 import styles from '~/styles/tailwind.css'
 import Navbar from './components/Navbar'
-
-export const links: LinksFunction = () => [{ rel: 'stylesheet', href: styles }]
+export const links: LinksFunction = () => [
+  { rel: 'stylesheet', href: styles },
+  {
+    rel: 'manifest',
+    href: '/manifest.json'
+  },
+  {
+    rel: 'icon',
+    type: 'image/png',
+    href: '/images/logo_192x192.png'
+  }
+]
 
 export const loader = (args: LoaderArgs) => rootAuthLoader(args)
 
@@ -23,7 +33,11 @@ function App() {
     <html lang='en'>
       <head>
         <meta charSet='utf-8' />
-        <meta name='viewport' content='width=device-width,initial-scale=1' />
+        <meta
+          name='viewport'
+          content='width=device-width,initial-scale=1, maximum-scale=1.0, user-scalable=0'
+        />
+        <meta name='HandheldFriendly' content='true' />
         <Meta />
         <Links />
       </head>
