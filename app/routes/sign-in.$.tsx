790da84acc9ab807b50c7e5111c67176d6d7e2f8
Sam Johnston <johnstonsam712@gmail.com>
2023-06-25 15:18:49 +0100

    fix styling on /sign-up and /sign-in routes

--- app/routes/sign-in.$.tsx
+++ app/routes/sign-in.$.tsx
@@ -2,7 +2,7 @@ import { SignIn } from '@clerk/remix'
 
 export default function SignInPage() {
   return (
-    <div className='flex justify-center items-center'>
+    <div className='flex justify-center items-center my-4'>
       <SignIn routing={'path'} path={'/sign-in'} />
     </div>
   )
--- app/routes/sign-up.$.tsx
+++ app/routes/sign-up.$.tsx
@@ -3,8 +3,7 @@ import { SignUp } from '@clerk/remix'
 
 export default function SignUpPage() {
   return (
-    <div>
-      <h1>Sign Up route</h1>
+    <div className='flex justify-center items-center my-4'>
       <SignUp routing={'path'} path={'/sign-up'} />
     </div>
   )
