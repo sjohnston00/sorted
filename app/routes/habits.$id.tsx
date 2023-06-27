b1baf48c03083f1e2bf1679a21960fa39c7ffa98
Sam Jonston <johnstonsam712@gmail.com>
2023-06-27 21:40:41 +0100

    select days for a habit

--- app/routes/habits.$id.tsx
+++ app/routes/habits.$id.tsx
@@ -1,7 +1,7 @@
-import { ActionArgs, LoaderArgs, redirect } from "@remix-run/node"
-import { Form, useLoaderData } from "@remix-run/react"
-import { prisma } from "~/db.server"
-import { getUser } from "~/utils/auth"
+import { ActionArgs, LoaderArgs, redirect } from '@remix-run/node'
+import { Form, useLoaderData } from '@remix-run/react'
+import { prisma } from '~/db.server'
+import { getUser } from '~/utils/auth'
 
 export const loader = async (args: LoaderArgs) => {
   const { userId } = await getUser(args)
@@ -9,19 +9,19 @@ export const loader = async (args: LoaderArgs) => {
 
   const habit = await prisma.habit.findUnique({
     where: {
-      id,
-    },
+      id
+    }
   })
 
   if (!habit || habit.deleted) {
-    throw new Response("Not Found", {
-      status: 404,
+    throw new Response('Not Found', {
+      status: 404
     })
   }
 
   if (habit.userId !== userId) {
-    throw new Response("Not Authorized", {
-      status: 401,
+    throw new Response('Not Authorized', {
+      status: 401
     })
   }
   return { habit }
@@ -33,55 +33,83 @@ export const action = async (args: ActionArgs) => {
 
   const habit = await prisma.habit.findUnique({
     where: {
-      id,
-    },
+      id
+    }
   })
 
   if (!habit || habit.deleted) {
-    throw new Response("Not Found", {
-      status: 404,
+    throw new Response('Not Found', {
+      status: 404
     })
   }
 
   if (habit.userId !== userId) {
-    throw new Response("Not Authorized", {
-      status: 401,
+    throw new Response('Not Authorized', {
+      status: 401
     })
   }
   await prisma.habit.update({
     data: {
       deleted: true,
-      deletedAt: new Date(),
+      deletedAt: new Date()
     },
     where: {
-      id,
-    },
+      id
+    }
   })
 
-  throw redirect("/habits")
+  throw redirect('/habits')
 }
 
 export default function Habit() {
   const { habit } = useLoaderData<typeof loader>()
   return (
-    <div className="grid grid-cols-4 gap-4">
+    <div className=''>
       <button
-        type="button"
-        className="py-4 px-2 rounded shadow text-center border-2 font-semibold text-lg tracking-wide disabled:opacity-30 disabled:cursor-not-allowed active:opacity-70 active:scale-90 transition"
+        type='button'
+        className='py-4 px-2 rounded shadow text-center border-2 font-semibold text-lg tracking-wide disabled:opacity-30 disabled:cursor-not-allowed active:opacity-70 active:scale-90 transition'
         style={
           {
             color: habit.colour,
             borderColor: habit.colour,
             backgroundColor: `${habit.colour}20`,
-            "--tw-shadow-color": habit.colour,
+            '--tw-shadow-color': habit.colour
           } as React.CSSProperties
         }>
         {habit.name}
       </button>
-      <Form method="post">
+      <span className='text-lg font-bold mt-4 mb-2 block'>Days</span>
+      <div className='flex gap-4 justify-center items-center my-4'>
+        {[
+          'Monday',
+          'Tuesday',
+          'Wednesday',
+          'Thursday',
+          'Friday',
+          'Saturday',
+          'Sunday'
+        ].map((d, i) => (
+          <label
+            htmlFor={`days-${d}`}
+            key={d}
+            className='h-8 w-8 flex justify-center items-center rounded-full relative text-center transition select-none z-[2] toggle-label border-2 text-sky-500 border-sky-500'>
+            {d.substring(0, 1)}
+            <input
+              type='checkbox'
+              name='days'
+              disabled
+              defaultChecked={habit.days.includes(d)}
+              id={`days-${d}`}
+              value={d}
+              className='appearance-none cursor-pointer absolute inset-0 rounded-full toggle-checkbox z-[1]'
+            />
+          </label>
+        ))}
+      </div>
+      <Form method='post'>
         <button
-          type="submit"
-          className="py-4 px-2 rounded shadow text-center disabled:opacity-30 disabled:cursor-not-allowed active:opacity-70 active:scale-90 transition bg-red-600 text-white">
+          type='submit'
+          className='py-4 px-2 rounded shadow text-center disabled:opacity-30 disabled:cursor-not-allowed active:opacity-70 active:scale-90 transition bg-red-600 text-white'>
           delete
         </button>
       </Form>
--- app/routes/habits.new.tsx
+++ app/routes/habits.new.tsx
@@ -1,7 +1,7 @@
-import { ActionArgs, LoaderArgs, redirect } from "@remix-run/node"
-import { Form, useNavigation } from "@remix-run/react"
-import { prisma } from "~/db.server"
-import { getUser } from "~/utils/auth"
+import { ActionArgs, LoaderArgs, redirect } from '@remix-run/node'
+import { Form, useNavigation } from '@remix-run/react'
+import { prisma } from '~/db.server'
+import { getUser } from '~/utils/auth'
 
 export const loader = async (args: LoaderArgs) => {
   await getUser(args)
@@ -17,39 +17,75 @@ export const action = async (args: ActionArgs) => {
       name: name.toString()!,
       colour: colour.toString()!,
       userId,
-    },
+      days: formData.getAll('days').map((d) => String(d))
+    }
   })
 
-  throw redirect("/habits")
+  throw redirect('/habits')
 }
 
 export default function NewHabit() {
   const navigation = useNavigation()
-  const isSubmitting = navigation.state === "submitting"
-  const isLoading = navigation.state === "loading"
+  const isSubmitting = navigation.state === 'submitting'
+  const isLoading = navigation.state === 'loading'
   return (
-    <div className="max-w-md mx-auto">
-      <Form method="post">
-        <input
-          className="block my-2 p-2 border border-gray-400"
-          type="text"
-          name="name"
-          id="name"
-          placeholder="name"
-          minLength={3}
-          maxLength={255}
-          required
-        />
-        <input
-          className="block my-2 p-1 border border-gray-400"
-          type="color"
-          name="colour"
-          id="colour"
-          required
-        />
+    <div className='max-w-md mx-auto'>
+      <Form method='post'>
+        <div className='flex gap-2'>
+          <div className='flex-1'>
+            <label htmlFor='name'>Name</label>
+            <input
+              className='block w-full p-2 border border-gray-400'
+              type='text'
+              name='name'
+              id='name'
+              placeholder='name'
+              autoComplete='off'
+              minLength={3}
+              maxLength={255}
+              required
+            />
+          </div>
+          <div className='flex-1'>
+            <label htmlFor='colour'>Colour</label>
+            <input
+              className='block w-full rounded-none border-none bg-none'
+              type='color'
+              name='colour'
+              id='colour'
+              required
+            />
+          </div>
+        </div>
+        <span className='text-lg font-bold mt-4 mb-2 block'>Days</span>
+        <div className='flex gap-4 justify-center items-center my-4'>
+          {[
+            'Monday',
+            'Tuesday',
+            'Wednesday',
+            'Thursday',
+            'Friday',
+            'Saturday',
+            'Sunday'
+          ].map((d, i) => (
+            <label
+              htmlFor={`days-${d}`}
+              key={d}
+              className='h-8 w-8 flex justify-center items-center rounded-full relative text-center transition select-none z-[2] toggle-label border-2 text-sky-500 border-sky-500'>
+              {d.substring(0, 1)}
+              <input
+                type='checkbox'
+                name='days'
+                id={`days-${d}`}
+                value={d}
+                className='appearance-none cursor-pointer absolute inset-0 rounded-full toggle-checkbox z-[1]'
+              />
+            </label>
+          ))}
+        </div>
         <button
-          type="submit"
-          className="px-4 py-2 bg-indigo-400 text-gray-100 font-semibold tracking-wide rounded shadow active:scale-95 active:opacity-80 disabled:opacity-80 disabled:cursor-not-allowed transition"
+          type='submit'
+          className='px-4 py-2 bg-indigo-400 text-gray-100 font-semibold tracking-wide rounded shadow active:scale-95 active:opacity-80 disabled:opacity-80 disabled:cursor-not-allowed transition'
           disabled={isSubmitting || isLoading}>
           Create
         </button>
