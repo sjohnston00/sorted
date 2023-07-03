9b74004ddc7480d02aee4d5f9d5a4a4f16497225
Sam Johnston <johnstonsam712@gmail.com>
2023-07-03 13:44:32 +0100

    add heroIcons

--- app/components/Calendar/HabitButton.tsx
+++ app/components/Calendar/HabitButton.tsx
@@ -1,8 +1,8 @@
-import { Habit } from '@prisma/client'
-import { SerializeFrom } from '@remix-run/node'
-import { useFetcher } from '@remix-run/react'
-import { format } from 'date-fns'
-import React from 'react'
+import { Habit } from "@prisma/client"
+import { SerializeFrom } from "@remix-run/node"
+import { useFetcher } from "@remix-run/react"
+import { format } from "date-fns"
+import React from "react"
 
 type HabitButtonProps = {
   habit: SerializeFrom<Habit>
@@ -11,29 +11,29 @@ type HabitButtonProps = {
 
 export default function HabitButton({ habit, selectedDay }: HabitButtonProps) {
   const fetcher = useFetcher()
-  const isSubmitting = fetcher.state === 'submitting'
+  const isSubmitting = fetcher.state === "submitting"
   return (
-    <fetcher.Form method='post' className='h-full w-full'>
-      <input type='hidden' name='_action' value='mark-date' />
+    <fetcher.Form method="post" className="h-full w-full">
+      <input type="hidden" name="_action" value="mark-date" />
       <input
-        type='hidden'
-        name='date'
-        value={format(selectedDay, 'yyyy-MM-dd')}
+        type="hidden"
+        name="date"
+        value={format(selectedDay, "yyyy-MM-dd")}
       />
-      <input type='hidden' name='habitName' value={habit.name} />
-      <input type='hidden' name='habitColour' value={habit.colour} />
+      <input type="hidden" name="habitName" value={habit.name} />
+      <input type="hidden" name="habitColour" value={habit.colour} />
       <button
-        className='min-h-[8rem] w-full py-4 min px-2 rounded shadow border-2 text-center font-semibold text-lg tracking-wide disabled:opacity-30 disabled:cursor-not-allowed active:opacity-70 active:scale-90 transition'
+        className="min-h-[8rem] w-full py-4 min px-2 rounded shadow border-2 text-center font-semibold text-lg tracking-wide disabled:opacity-30 disabled:cursor-not-allowed active:opacity-70 active:scale-90 transition"
         style={
           {
             color: habit.colour,
             borderColor: habit.colour,
             backgroundColor: `${habit.colour}20`,
-            '--tw-shadow-color': habit.colour
+            "--tw-shadow-color": habit.colour,
           } as React.CSSProperties
         }
-        type='submit'
-        name='habitId'
+        type="submit"
+        name="habitId"
         value={habit.id}
         disabled={isSubmitting}>
         {habit.name}
--- app/routes/habits.new.tsx
+++ app/routes/habits.new.tsx
@@ -1,7 +1,8 @@
-import { ActionArgs, LoaderArgs, redirect } from '@remix-run/node'
-import { Form, useNavigation } from '@remix-run/react'
-import { prisma } from '~/db.server'
-import { getUser } from '~/utils/auth'
+// import * as Icons from "@heroicons/react/24/outline"
+import { ActionArgs, LoaderArgs, redirect } from "@remix-run/node"
+import { Form, useNavigation } from "@remix-run/react"
+import { prisma } from "~/db.server"
+import { getUser } from "~/utils/auth"
 
 export const loader = async (args: LoaderArgs) => {
   await getUser(args)
@@ -17,78 +18,93 @@ export const action = async (args: ActionArgs) => {
       name: name.toString()!,
       colour: colour.toString()!,
       userId,
-      days: formData.getAll('days').map((d) => String(d))
-    }
+      days: formData.getAll("days").map((d) => String(d)),
+    },
   })
 
-  throw redirect('/habits')
+  throw redirect("/habits")
 }
 
 export default function NewHabit() {
   const navigation = useNavigation()
-  const isSubmitting = navigation.state === 'submitting'
-  const isLoading = navigation.state === 'loading'
+  const isSubmitting = navigation.state === "submitting"
+  const isLoading = navigation.state === "loading"
+  // const icons = Object.entries(Icons)
+
   return (
-    <div className='max-w-md mx-auto'>
-      <Form method='post'>
-        <div className='flex gap-2'>
-          <div className='flex-1'>
-            <label htmlFor='name'>Name</label>
+    <div className="max-w-md mx-auto">
+      <Form method="post">
+        <div className="flex gap-2">
+          <div className="flex-1">
+            <label htmlFor="name">Name</label>
             <input
-              className='block w-full p-2 border border-gray-400'
-              type='text'
-              name='name'
-              id='name'
-              placeholder='name'
-              autoComplete='off'
+              className="block w-full p-2 border border-gray-400"
+              type="text"
+              name="name"
+              id="name"
+              placeholder="name"
+              autoComplete="off"
               minLength={3}
               maxLength={255}
               required
             />
           </div>
-          <div className='flex-1'>
-            <label htmlFor='colour'>Colour</label>
+          <div className="flex-1">
+            <label htmlFor="colour">Colour</label>
             <input
-              className='block w-full rounded-none border-none bg-none'
-              type='color'
-              name='colour'
-              id='colour'
+              className="block w-full rounded-none border-none bg-none"
+              type="color"
+              name="colour"
+              id="colour"
               required
             />
           </div>
         </div>
-        <span className='text-sm text-center text-gray-400 mt-4 mb-2 block'>
+        <span className="text-sm text-center text-gray-400 mt-4 mb-2 block">
           Select the days you'd like to track:
         </span>
-        <div className='grid grid-cols-7 gap-1 md:gap-3 justify-items-center my-4'>
+        <div className="grid grid-cols-7 gap-1 md:gap-3 justify-items-center my-4">
           {[
-            'Monday',
-            'Tuesday',
-            'Wednesday',
-            'Thursday',
-            'Friday',
-            'Saturday',
-            'Sunday'
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday",
+            "Saturday",
+            "Sunday",
           ].map((d, i) => (
             <label
               htmlFor={`days-${d}`}
               key={d}
               title={d}
-              className='scale-95 min-h-[2rem] h-full w-full aspect-square flex justify-center items-center rounded-full relative text-center transition select-none z-[2] toggle-label border-2 text-sky-500 border-sky-500'>
+              className="scale-95 min-h-[2rem] h-full w-full aspect-square flex justify-center items-center rounded-full relative text-center transition select-none z-[2] toggle-label border-2 text-sky-500 border-sky-500">
               {d.substring(0, 1)}
               <input
-                type='checkbox'
-                name='days'
+                type="checkbox"
+                name="days"
                 id={`days-${d}`}
                 value={d}
-                className='appearance-none cursor-pointer absolute inset-0 rounded-full toggle-checkbox z-[1]'
+                className="appearance-none cursor-pointer absolute inset-0 rounded-full toggle-checkbox z-[1]"
               />
             </label>
           ))}
         </div>
+        {/* 
+        TODO: Allow users to select an icon or randomise them
+        <div className="grid grid-cols-6 gap-4">
+          {icons.map(([name, Component]) => (
+            <div
+              className="flex flex-col gap-2 justify-center items-center"
+              key={name}
+              title={name}>
+              <Component className="h-6 w-6" />
+              <span>{name}</span>
+            </div>
+          ))}
+        </div> */}
         <button
-          type='submit'
-          className='px-4 py-2 bg-indigo-400 text-gray-100 font-semibold tracking-wide rounded shadow active:scale-95 active:opacity-80 disabled:opacity-80 disabled:cursor-not-allowed transition'
+          type="submit"
+          className="px-4 py-2 bg-indigo-400 text-gray-100 font-semibold tracking-wide rounded shadow active:scale-95 active:opacity-80 disabled:opacity-80 disabled:cursor-not-allowed transition"
           disabled={isSubmitting || isLoading}>
           Create
         </button>
