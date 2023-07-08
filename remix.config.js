e643cbacce4278e0f3f0cdb63bac867c0c1c7691
Sam Johnston <johnstonsam712@gmail.com>
2023-07-08 19:09:26 +0100

    update deps

--- remix.config.js
+++ remix.config.js
@@ -9,7 +9,7 @@ module.exports = {
   tailwind: true,
   future: {
     v2_dev: true,
-    v2_errorBoundary: false,
+    v2_errorBoundary: true,
     v2_headers: true,
     v2_meta: true,
     v2_normalizeFormMethod: true,
