e7acde1fca0ddd454262436979279b37ee9dcf10
Sam Johnston <johnstonsam712@gmail.com>
2023-07-25 15:21:21 +0100

    add remix development tools package

--- app/root.tsx
+++ app/root.tsx
@@ -1,54 +1,60 @@
-import {
-  ClerkApp,
-  ClerkCatchBoundary,
-  V2_ClerkErrorBoundary
-} from '@clerk/remix/'
-import { rootAuthLoader } from '@clerk/remix/ssr.server'
-import type { LinksFunction, LoaderArgs } from '@remix-run/node'
+import { ClerkApp, V2_ClerkErrorBoundary } from "@clerk/remix/"
+import { rootAuthLoader } from "@clerk/remix/ssr.server"
+import type { LinksFunction, LoaderArgs } from "@remix-run/node"
 import {
   Links,
   LiveReload,
   Meta,
   Outlet,
   Scripts,
-  ScrollRestoration
-} from '@remix-run/react'
-import styles from '~/styles/tailwind.css'
-import Navbar from './components/Navbar'
+  ScrollRestoration,
+} from "@remix-run/react"
+import React from "react"
+import rdtStylesheet from "remix-development-tools/stylesheet.css"
+import styles from "~/styles/tailwind.css"
+import Navbar from "./components/Navbar"
+const RemixDevTools =
+  process.env.NODE_ENV === "development"
+    ? React.lazy(() => import("remix-development-tools"))
+    : undefined
 export const links: LinksFunction = () => [
-  { rel: 'stylesheet', href: styles },
+  { rel: "stylesheet", href: styles },
   {
-    rel: 'manifest',
-    href: '/manifest.json'
+    rel: "manifest",
+    href: "/manifest.json",
   },
   {
-    rel: 'icon',
-    type: 'image/png',
-    href: '/images/logo_192x192.png'
-  }
+    rel: "icon",
+    type: "image/png",
+    href: "/images/logo_192x192.png",
+  },
+  ...(rdtStylesheet && process.env.NODE_ENV === "development"
+    ? [{ rel: "stylesheet", href: rdtStylesheet }]
+    : []),
 ]
 
 export const loader = (args: LoaderArgs) => rootAuthLoader(args)
 
 function App() {
   return (
-    <html lang='en'>
+    <html lang="en">
       <head>
-        <meta charSet='utf-8' />
+        <meta charSet="utf-8" />
         <meta
-          name='viewport'
-          content='width=device-width,initial-scale=1, maximum-scale=1.0, user-scalable=0'
+          name="viewport"
+          content="width=device-width,initial-scale=1, maximum-scale=1.0, user-scalable=0"
         />
-        <meta name='HandheldFriendly' content='true' />
+        <meta name="HandheldFriendly" content="true" />
         <Meta />
         <Links />
       </head>
-      <body className='font-sans'>
+      <body className="font-sans">
         <Navbar />
         <Outlet />
         <ScrollRestoration />
+        {process.env.NODE_ENV === "development" ? <LiveReload /> : null}
+        {RemixDevTools && <RemixDevTools />}
         <Scripts />
-        <LiveReload />
       </body>
     </html>
   )
