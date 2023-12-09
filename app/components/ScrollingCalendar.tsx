e6f1feadb5515c51b2caa89a2b8c7e2efa0ea2cc
Sam Jonston <johnstonsam712@gmail.com>
2023-12-09 17:32:45 +0000

    optomised scrolling calendar component to filter markedHabits based on month as each button click was taking 250ms+ to render 😱 create <ReactVirtualizedScrollingCalendar/> dev component

--- app/components/ScrollingCalendar.tsx
+++ app/components/ScrollingCalendar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
 import {
   addMonths,
   eachDayOfInterval,
@@ -14,69 +14,80 @@ import {
   parseISO,
   startOfToday,
   startOfWeek,
-  subMonths,
-} from "date-fns";
-import { classNames } from "~/utils";
-import { twMerge } from "tailwind-merge";
+  subMonths
+} from 'date-fns'
+import { classNames } from '~/utils'
+import { twMerge } from 'tailwind-merge'
+import { AutoSizer, List } from 'react-virtualized'
 
 type ScrollingCalendarProps = {
-  indicators?: any[];
-  monthsPrevious?: number;
-  monthsNext?: number;
-};
+  indicators?: any[]
+  monthsPrevious?: number
+  monthsNext?: number
+}
 
 export default function ScrollingCalendar({
   indicators,
   monthsPrevious = 5,
-  monthsNext = 2,
+  monthsNext = 2
 }: ScrollingCalendarProps) {
   //   const { markedHabits } = useLoaderData<typeof loader>();
-  const today = startOfToday();
-  const currentMonth = format(today, "MMM-yyyy");
-  const firstDayCurrentMonth = parse(currentMonth, "MMM-yyyy", new Date());
-  const previousMonths: Date[] = [];
-  const [selectedDay, setSelectedDay] = useState(today);
+  const today = startOfToday()
+  const currentMonth = format(today, 'MMM-yyyy')
+  const firstDayCurrentMonth = parse(currentMonth, 'MMM-yyyy', new Date())
+  const previousMonths: Date[] = []
+  const [selectedDay, setSelectedDay] = useState(today)
 
   for (let index = monthsPrevious; index > 0; index--) {
-    previousMonths.push(subMonths(firstDayCurrentMonth, index));
+    previousMonths.push(subMonths(firstDayCurrentMonth, index))
   }
 
-  const nextMonths: Date[] = [];
+  const nextMonths: Date[] = []
   for (let index = 0; index < monthsNext; index++) {
-    nextMonths.push(addMonths(firstDayCurrentMonth, index + 1));
+    nextMonths.push(addMonths(firstDayCurrentMonth, index + 1))
   }
 
-  const monhts = [...previousMonths, firstDayCurrentMonth, ...nextMonths];
+  const monhts = [...previousMonths, firstDayCurrentMonth, ...nextMonths]
 
   let colStartClasses = [
-    "",
-    "col-start-2",
-    "col-start-3",
-    "col-start-4",
-    "col-start-5",
-    "col-start-6",
-    "col-start-7",
-  ];
+    '',
+    'col-start-2',
+    'col-start-3',
+    'col-start-4',
+    'col-start-5',
+    'col-start-6',
+    'col-start-7'
+  ]
   return (
-    <div className="flex gap-8 overflow-x-auto snap-x snap-mandatory pb-4 mb-20 [-webkit-overflow-scrolling:touch] scroll-smooth">
+    <div className='flex gap-8 overflow-x-auto snap-x snap-mandatory pb-4 mb-20 [-webkit-overflow-scrolling:touch] scroll-smooth'>
       {monhts.map((month, i) => {
         const days = eachDayOfInterval({
           start: startOfWeek(month, {
-            weekStartsOn: 1,
+            weekStartsOn: 1
           }),
           end: endOfWeek(endOfMonth(month), {
-            weekStartsOn: 1,
-          }),
-        });
+            weekStartsOn: 1
+          })
+        })
+        const monthIndicators = indicators?.filter((m) =>
+          isSameMonth(parseISO(m.date), month)
+        )
         return (
+          // TODO: Get `react-virtualized` working
+          // https://github.com/bvaughn/react-virtualized/issues/1632
+          // Maybe try using <Grid/> instead of <List/> for horizontal scrolling as suggested in
+          //
+          // https://stackoverflow.com/questions/46177344/react-virtualized-table-x-scrolling
+          //
+          //
+          // <ReactVirtualizedScrollingCalendar indicators={indicators} monthsNext={monthsNext} monthsPrevious={monthsPrevious}/>
           <div
             key={month.getTime()}
-            className="[scroll-snap-align:start] snap-always min-w-full min-h-full"
-          >
-            <span className="text-xl font-bold ml-2">
-              {format(month, "MMMM yyyy")}
+            className='[scroll-snap-align:start] snap-always min-w-full min-h-full'>
+            <span className='text-xl font-bold ml-2'>
+              {format(month, 'MMMM yyyy')}
             </span>
-            <div className="grid grid-cols-7 mt-10 text-xs leading-6 text-center text-gray-500">
+            <div className='grid grid-cols-7 mt-10 text-xs leading-6 text-center text-gray-500'>
               <div>M</div>
               <div>T</div>
               <div>W</div>
@@ -85,70 +96,207 @@ export default function ScrollingCalendar({
               <div>S</div>
               <div>S</div>
             </div>
-            <div className="grid grid-cols-7 mt-2 text-sm">
-              {days.map((day, dayIdx) => (
-                <div
-                  key={day.toString()}
-                  className={classNames(
-                    dayIdx === 0 && colStartClasses[getDay(day) - 1],
-                    "py-1.5"
-                  )}
-                >
-                  <button
-                    type="submit"
-                    name="date"
-                    value={format(day, "yyyy-MM-dd")}
-                    className={twMerge(
-                      "mx-auto flex h-8 w-8 items-center justify-center rounded-full",
-                      isEqual(day, selectedDay) && "text-white",
-                      !isEqual(day, selectedDay) &&
-                        isToday(day) &&
-                        "text-red-500",
-                      !isEqual(day, selectedDay) &&
-                        !isToday(day) &&
-                        isSameMonth(day, firstDayCurrentMonth) &&
-                        "text-gray-900 dark:text-gray-300",
-                      !isEqual(day, selectedDay) &&
-                        !isToday(day) &&
-                        !isSameMonth(day, firstDayCurrentMonth) &&
-                        "text-gray-400",
-                      isEqual(day, selectedDay) && isToday(day) && "bg-red-500",
-                      isEqual(day, selectedDay) &&
-                        !isToday(day) &&
-                        "bg-gray-900",
-                      !isEqual(day, selectedDay) &&
-                        "hover:bg-gray-200 dark:hover:bg-gray-700",
-                      (isEqual(day, selectedDay) || isToday(day)) &&
-                        "font-semibold"
-                    )}
-                    onClick={() => {
-                      setSelectedDay(day);
-                    }}
-                  >
-                    <time dateTime={format(day, "yyyy-MM-dd")}>
-                      {format(day, "d")}
-                    </time>
-                  </button>
-                  <div className="flex h-1 w-fit gap-px mx-auto mt-1">
-                    {indicators
-                      ?.filter((m) => isSameDay(parseISO(m.date), day))
-                      .slice(0, 3)
-                      .map((m) => (
+            <div className='grid grid-cols-7 mt-2 text-sm'>
+              {days.map((day, dayIdx) => {
+                const dayIndicators = monthIndicators
+                  ?.filter((m) => isSameDay(parseISO(m.date), day))
+                  .slice(0, 3)
+                return (
+                  <div
+                    key={day.toString()}
+                    className={classNames(
+                      dayIdx === 0 && colStartClasses[getDay(day) - 1],
+                      'py-1.5'
+                    )}>
+                    <button
+                      type='submit'
+                      name='date'
+                      value={format(day, 'yyyy-MM-dd')}
+                      className={twMerge(
+                        'mx-auto flex h-8 w-8 items-center justify-center rounded-full',
+                        isEqual(day, selectedDay) && 'text-white',
+                        !isEqual(day, selectedDay) &&
+                          isToday(day) &&
+                          'text-red-500',
+                        !isEqual(day, selectedDay) &&
+                          !isToday(day) &&
+                          isSameMonth(day, firstDayCurrentMonth) &&
+                          'text-gray-900 dark:text-gray-300',
+                        !isEqual(day, selectedDay) &&
+                          !isToday(day) &&
+                          !isSameMonth(day, firstDayCurrentMonth) &&
+                          'text-gray-400',
+                        isEqual(day, selectedDay) &&
+                          isToday(day) &&
+                          'bg-red-500',
+                        isEqual(day, selectedDay) &&
+                          !isToday(day) &&
+                          'bg-gray-900',
+                        !isEqual(day, selectedDay) &&
+                          'hover:bg-gray-200 dark:hover:bg-gray-700',
+                        (isEqual(day, selectedDay) || isToday(day)) &&
+                          'font-semibold'
+                      )}
+                      onClick={() => {
+                        setSelectedDay(day)
+                      }}>
+                      <time dateTime={format(day, 'yyyy-MM-dd')}>
+                        {format(day, 'd')}
+                      </time>
+                    </button>
+                    <div className='flex h-1 w-fit gap-px mx-auto mt-1'>
+                      {dayIndicators?.map((m) => (
                         <div
                           key={`calendar-${m.id}`}
-                          className="w-1 h-1 rounded-full"
+                          className='w-1 h-1 rounded-full'
                           style={{
-                            backgroundColor: m.habit.colour,
-                          }}
-                        ></div>
+                            backgroundColor: m.habit.colour
+                          }}></div>
                       ))}
+                    </div>
                   </div>
-                </div>
-              ))}
+                )
+              })}
             </div>
           </div>
-        );
+        )
       })}
     </div>
-  );
+  )
+}
+
+function ReactVirtualizedScrollingCalendar({
+  indicators,
+  monthsNext = 5,
+  monthsPrevious = 2
+}: ScrollingCalendarProps) {
+  //   const { markedHabits } = useLoaderData<typeof loader>();
+  const today = startOfToday()
+  const currentMonth = format(today, 'MMM-yyyy')
+  const firstDayCurrentMonth = parse(currentMonth, 'MMM-yyyy', new Date())
+  const previousMonths: Date[] = []
+  const [selectedDay, setSelectedDay] = useState(today)
+
+  for (let index = monthsPrevious; index > 0; index--) {
+    previousMonths.push(subMonths(firstDayCurrentMonth, index))
+  }
+
+  const nextMonths: Date[] = []
+  for (let index = 0; index < monthsNext; index++) {
+    nextMonths.push(addMonths(firstDayCurrentMonth, index + 1))
+  }
+
+  const monhts = [...previousMonths, firstDayCurrentMonth, ...nextMonths]
+
+  let colStartClasses = [
+    '',
+    'col-start-2',
+    'col-start-3',
+    'col-start-4',
+    'col-start-5',
+    'col-start-6',
+    'col-start-7'
+  ]
+  return (
+    <AutoSizer>
+      {({ height, width }) => (
+        <List
+          height={height}
+          rowCount={monhts.length}
+          width={width}
+          rowHeight={300}
+          className='flex gap-8 overflow-x-auto snap-x snap-mandatory pb-4 mb-20 [-webkit-overflow-scrolling:touch] scroll-smooth'
+          rowRenderer={({ key, index, style }) => {
+            const month = monhts[index]
+            const days = eachDayOfInterval({
+              start: startOfWeek(month, {
+                weekStartsOn: 1
+              }),
+              end: endOfWeek(endOfMonth(month), {
+                weekStartsOn: 1
+              })
+            })
+            return (
+              <div key={key} style={style}>
+                <div className='[scroll-snap-align:start] snap-always min-w-full min-h-full'>
+                  <span className='text-xl font-bold ml-2'>
+                    {format(month, 'MMMM yyyy')}
+                  </span>
+                  <div className='grid grid-cols-7 mt-10 text-xs leading-6 text-center text-gray-500'>
+                    <div>M</div>
+                    <div>T</div>
+                    <div>W</div>
+                    <div>T</div>
+                    <div>F</div>
+                    <div>S</div>
+                    <div>S</div>
+                  </div>
+                  <div className='grid grid-cols-7 mt-2 text-sm'>
+                    {days.map((day, dayIdx) => (
+                      <div
+                        key={day.toString()}
+                        className={classNames(
+                          dayIdx === 0 && colStartClasses[getDay(day) - 1],
+                          'py-1.5'
+                        )}>
+                        <button
+                          type='submit'
+                          name='date'
+                          value={format(day, 'yyyy-MM-dd')}
+                          className={twMerge(
+                            'mx-auto flex h-8 w-8 items-center justify-center rounded-full',
+                            isEqual(day, selectedDay) && 'text-white',
+                            !isEqual(day, selectedDay) &&
+                              isToday(day) &&
+                              'text-red-500',
+                            !isEqual(day, selectedDay) &&
+                              !isToday(day) &&
+                              isSameMonth(day, firstDayCurrentMonth) &&
+                              'text-gray-900 dark:text-gray-300',
+                            !isEqual(day, selectedDay) &&
+                              !isToday(day) &&
+                              !isSameMonth(day, firstDayCurrentMonth) &&
+                              'text-gray-400',
+                            isEqual(day, selectedDay) &&
+                              isToday(day) &&
+                              'bg-red-500',
+                            isEqual(day, selectedDay) &&
+                              !isToday(day) &&
+                              'bg-gray-900',
+                            !isEqual(day, selectedDay) &&
+                              'hover:bg-gray-200 dark:hover:bg-gray-700',
+                            (isEqual(day, selectedDay) || isToday(day)) &&
+                              'font-semibold'
+                          )}
+                          onClick={() => {
+                            setSelectedDay(day)
+                          }}>
+                          <time dateTime={format(day, 'yyyy-MM-dd')}>
+                            {format(day, 'd')}
+                          </time>
+                        </button>
+                        <div className='flex h-1 w-fit gap-px mx-auto mt-1'>
+                          {indicators
+                            ?.filter((m) => isSameDay(parseISO(m.date), day))
+                            .slice(0, 3)
+                            .map((m) => (
+                              <div
+                                key={`calendar-${m.id}`}
+                                className='w-1 h-1 rounded-full'
+                                style={{
+                                  backgroundColor: m.habit.colour
+                                }}></div>
+                            ))}
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            )
+          }}
+        />
+      )}
+    </AutoSizer>
+  )
 }
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,56 +1,43 @@
-import { useUser } from "@clerk/remix";
-import { Habit, MarkedHabit } from "@prisma/client";
+import { useUser } from '@clerk/remix'
+import { Habit, MarkedHabit } from '@prisma/client'
 import {
   ActionFunctionArgs,
   redirect,
   type LoaderFunctionArgs,
-  type MetaFunction,
-} from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
-import {
-  eachDayOfInterval,
-  endOfMonth,
-  endOfWeek,
-  format,
-  getDay,
-  isSameDay,
-  isSameMonth,
-  isToday,
-  parse,
-  parseISO,
-  startOfToday,
-  startOfWeek,
-} from "date-fns";
-import { z } from "zod";
-import Calendar from "~/components/Calendar";
-import FriendsRow from "~/components/FriendsRow";
-import ScrollingCalendar from "~/components/ScrollingCalendar";
-import { prisma } from "~/db.server";
-import { classNames, getClerkUser } from "~/utils";
-import { getUser } from "~/utils/auth.server";
+  type MetaFunction
+} from '@remix-run/node'
+import { useLoaderData } from '@remix-run/react'
+import { format } from 'date-fns'
+import { z } from 'zod'
+import Calendar from '~/components/Calendar'
+import FriendsRow from '~/components/FriendsRow'
+import ScrollingCalendar from '~/components/ScrollingCalendar'
+import { prisma } from '~/db.server'
+import { getClerkUser } from '~/utils'
+import { getUser } from '~/utils/auth.server'
 
 type LoaderData = {
   markedHabits: (MarkedHabit & {
-    habit: Habit;
-  })[];
-  habits: Habit[];
-  isLoadingFriendsHabits: boolean;
-};
+    habit: Habit
+  })[]
+  habits: Habit[]
+  isLoadingFriendsHabits: boolean
+}
 
 export const loader = async (args: LoaderFunctionArgs): Promise<LoaderData> => {
-  const { userId } = await getUser(args);
+  const { userId } = await getUser(args)
 
-  const url = new URL(args.request.url);
+  const url = new URL(args.request.url)
   const { friend } = z
     .object({
-      friend: z.string().optional(),
+      friend: z.string().optional()
     })
-    .parse(Object.fromEntries(url.searchParams));
+    .parse(Object.fromEntries(url.searchParams))
 
   if (friend) {
-    const friendData = await getClerkUser(friend);
+    const friendData = await getClerkUser(friend)
     if (!friendData) {
-      throw redirect("/");
+      throw redirect('/')
     }
 
     const userFriend = await prisma.userFriends
@@ -60,27 +47,27 @@ export const loader = async (args: LoaderFunctionArgs): Promise<LoaderData> => {
             {
               AND: {
                 friendIdFrom: friendData.id,
-                friendIdTo: userId,
-              },
+                friendIdTo: userId
+              }
             },
             {
               AND: {
                 friendIdTo: friendData.id,
-                friendIdFrom: userId,
-              },
-            },
-          ],
-        },
+                friendIdFrom: userId
+              }
+            }
+          ]
+        }
       })
       .catch(() => {
-        console.log("users are not friends");
-        throw redirect("/");
-      });
+        console.log('users are not friends')
+        throw redirect('/')
+      })
 
     const userFriendId =
       userFriend.friendIdFrom === userId
         ? userFriend.friendIdTo
-        : userFriend.friendIdFrom;
+        : userFriend.friendIdFrom
 
     const [friendsMarkedHabits, friendsHabits] = await Promise.all([
       prisma.markedHabit.findMany({
@@ -88,30 +75,30 @@ export const loader = async (args: LoaderFunctionArgs): Promise<LoaderData> => {
           userId: userFriendId,
           habit: {
             deleted: false,
-            private: false,
-          },
+            private: false
+          }
         },
         include: {
-          habit: true,
+          habit: true
         },
         orderBy: {
-          date: "desc",
-        },
+          date: 'desc'
+        }
       }),
       prisma.habit.findMany({
         where: {
           userId: userFriendId,
           deleted: false,
-          private: false,
-        },
-      }),
-    ]);
+          private: false
+        }
+      })
+    ])
 
     return {
       markedHabits: friendsMarkedHabits,
       habits: friendsHabits,
-      isLoadingFriendsHabits: true,
-    };
+      isLoadingFriendsHabits: true
+    }
   }
 
   const [markedHabits, habits] = await Promise.all([
@@ -119,82 +106,82 @@ export const loader = async (args: LoaderFunctionArgs): Promise<LoaderData> => {
       where: {
         userId,
         habit: {
-          deleted: false,
-        },
+          deleted: false
+        }
       },
       include: {
-        habit: true,
+        habit: true
       },
       orderBy: {
-        date: "desc",
-      },
+        date: 'desc'
+      }
     }),
     prisma.habit.findMany({
       where: {
         userId,
-        deleted: false,
-      },
-    }),
-  ]);
+        deleted: false
+      }
+    })
+  ])
 
-  return { markedHabits, habits, isLoadingFriendsHabits: false };
-};
+  return { markedHabits, habits, isLoadingFriendsHabits: false }
+}
 
 export const action = async (args: ActionFunctionArgs) => {
-  const { userId } = await getUser(args);
+  const { userId } = await getUser(args)
 
-  const formData = await args.request.formData();
-  if (formData.get("_action") === "mark-date") {
+  const formData = await args.request.formData()
+  if (formData.get('_action') === 'mark-date') {
     await prisma.markedHabit.create({
       data: {
         date: new Date(
-          `${formData.get("date")?.toString()}T${format(new Date(), "HH:mm")}`
+          `${formData.get('date')?.toString()}T${format(new Date(), 'HH:mm')}`
         ),
         userId,
-        habitId: formData.get("habitId")?.toString()!,
-      },
-    });
-    return Object.fromEntries(formData);
+        habitId: formData.get('habitId')?.toString()!
+      }
+    })
+    return Object.fromEntries(formData)
   }
 
-  if (formData.get("_action") === "remove-marked-habit") {
+  if (formData.get('_action') === 'remove-marked-habit') {
     await prisma.markedHabit.delete({
       where: {
-        id: formData.get("markedHabit-id")?.toString(),
-      },
-    });
-    return {};
+        id: formData.get('markedHabit-id')?.toString()
+      }
+    })
+    return {}
   }
 
   await prisma.habit.create({
     data: {
       userId,
-      name: "Test",
-      colour: "#ffffff",
-    },
-  });
+      name: 'Test',
+      colour: '#ffffff'
+    }
+  })
 
-  return {};
-};
+  return {}
+}
 
 export const meta: MetaFunction = () => {
-  return [{ title: "Sorted" }];
-};
+  return [{ title: 'Sorted' }]
+}
 
 export default function Index() {
-  const { isLoaded, isSignedIn } = useUser();
+  const { isLoaded, isSignedIn } = useUser()
   const { markedHabits, habits, isLoadingFriendsHabits } =
-    useLoaderData<typeof loader>();
+    useLoaderData<typeof loader>()
 
   if (!isLoaded || !isSignedIn) {
-    return null;
+    return null
   }
 
   return (
     <div>
-      <div className="mt-8">
-        <div className="max-w-md px-4 mx-auto sm:px-7 md:max-w-4xl md:px-6 mb-8">
-          <h2 className="text-lg font-bold tracking-tight mb-4">Friends</h2>
+      <div className='mt-8'>
+        <div className='max-w-md px-4 mx-auto sm:px-7 md:max-w-4xl md:px-6 mb-8'>
+          <h2 className='text-lg font-bold tracking-tight mb-4'>Friends</h2>
           <FriendsRow />
 
           {/* <ScrollingCalendar indicators={markedHabits} /> */}
@@ -207,5 +194,5 @@ export default function Index() {
         </div>
       </div>
     </div>
-  );
+  )
 }
