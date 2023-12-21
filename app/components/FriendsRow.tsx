146c573076d25e0292d48ae028e8f2a479f48f38
Sam Jonston <johnstonsam712@gmail.com>
2023-12-21 20:48:01 +0000

    add a back button to the friend rows whenever a user is viewing a friend page

--- app/components/FriendsRow.tsx
+++ app/components/FriendsRow.tsx
@@ -1,15 +1,38 @@
 import React from 'react'
-import { useRouteLoaderData, useSearchParams } from '@remix-run/react'
+import {
+  Link,
+  useLoaderData,
+  useRouteLoaderData,
+  useSearchParams
+} from '@remix-run/react'
 import { RootLoaderData } from '~/root'
 import { useUser } from '@clerk/remix'
+import { IndexLoaderData } from '~/routes/_index'
+import { ArrowLeftIcon } from '@heroicons/react/24/outline'
+import { twMerge } from 'tailwind-merge'
 
 export default function FriendsRow() {
   const { user, isLoaded, isSignedIn } = useUser()
   const loggedInUser = useRouteLoaderData<RootLoaderData>('root')
   const [searchParams, setSearchParams] = useSearchParams()
+  const { isLoadingFriendsHabits } = useLoaderData<IndexLoaderData>()
+  const buttonClassName =
+    'h-24 w-16 flex flex-col justify-end gap-1 items-center'
 
   return (
-    <div className='mb-8 flex gap-2'>
+    <div className='mb-8 flex items-end gap-4'>
+      {isLoadingFriendsHabits ? (
+        <Link
+          to={'/'}
+          className={twMerge(buttonClassName, 'text-base-content')}
+          preventScrollReset
+          replace>
+          <div className='bg-base-200 flex justify-center items-center h-16 rounded-full w-full'>
+            <ArrowLeftIcon className='w-5 h-5' />
+          </div>
+          <div className='text-xs'>Back</div>
+        </Link>
+      ) : null}
       {loggedInUser?.friends && loggedInUser.friends.length > 0
         ? loggedInUser.friends.map((f) => {
             const friend = f.friendIdFrom === user?.id ? f.userTo : f.userFrom
@@ -17,7 +40,7 @@ export default function FriendsRow() {
               searchParams.get('friend') === friend?.username
             return (
               <button
-                className={`h-24 w-16 flex flex-col justify-end gap-1 items-center`}
+                className={buttonClassName}
                 key={f.id}
                 onClick={() => {
                   setSearchParams(
@@ -45,7 +68,7 @@ export default function FriendsRow() {
                 />
                 <span
                   className={`text-xs w-full overflow-ellipsis font-medium text-gray-400 ${
-                    searchParamSameFriend ? 'animate-pulse' : ''
+                    searchParamSameFriend ? 'duration-200 animate-blink ' : ''
                   }`}>
                   {friend?.username}
                 </span>
