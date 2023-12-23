b6260ce9babca48a2aa692e24b3ac91c788073f6
Sam Jonston <johnstonsam712@gmail.com>
2023-12-23 22:54:26 +0000

    install tailwind typegraphy

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,22 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 const config: Config = {
-  content: ["./app/**/*.{ts,tsx,jsx,js}"],
+  content: ['./app/**/*.{ts,tsx,jsx,js}'],
   theme: {
     extend: {
       fontSize: {
-        "2xs": ".625rem",
+        '2xs': '.625rem'
       },
       transitionDuration: {
-        "2000": "2000ms",
-        "5000": "5000ms",
-      },
-    },
+        '2000': '2000ms',
+        '5000': '5000ms'
+      }
+    }
   },
-  plugins: [require("tailwindcss-animate"), require("daisyui")],
-};
+  plugins: [
+    require('tailwindcss-animate'),
+    require('@tailwindcss/typography'),
+    require('daisyui')
+  ]
+}
 
-export default config;
+export default config
