477cfd3186a996c881ba5be5be01aa78cf5f2cea
Sam Jonston <johnstonsam712@gmail.com>
2023-12-26 17:37:21 +0000

    add tabs on trends page with tabs for 'week' 'month' and 'all-time' with

--- app/routes/trends/route.tsx
+++ app/routes/trends/route.tsx
@@ -1,18 +1,54 @@
+import { LockClosedIcon } from '@heroicons/react/24/outline'
 import type { LoaderFunctionArgs } from '@remix-run/node'
-import { useLoaderData } from '@remix-run/react'
-import { endOfMonth, startOfMonth } from 'date-fns'
+import { Link, useLoaderData } from '@remix-run/react'
+import { endOfMonth, endOfWeek, startOfMonth, startOfWeek } from 'date-fns'
 import React from 'react'
+import { z } from 'zod'
 import { prisma } from '~/db.server'
 import { getUser } from '~/utils/auth.server'
+import { FEATURE_FLAGS } from '~/utils/constants'
+import { getFeatureFlagEnabledWithDefaultValue } from '~/utils/featureFlags'
+import { UserFeatureFlagQueries } from '~/utils/queries.server'
 
 export const loader = async (args: LoaderFunctionArgs) => {
   const { userId } = await getUser(args)
 
-  //get startOfcurrentMonth with date-fns
-  const startOfCurrentMonth = startOfMonth(new Date())
-  const endOfCurrentMonth = endOfMonth(new Date())
+  const url = new URL(args.request.url)
+  const urlSearchSchema = z.object({
+    tab: z
+      .literal('week')
+      .or(z.literal('month'))
+      .or(z.literal('all-time'))
+      .optional()
+      .default('month')
+  })
+
+  const { tab } = urlSearchSchema.parse(Object.fromEntries(url.searchParams))
+
+  const userFeatureFlags = await UserFeatureFlagQueries.getUsersFeatureFlags(
+    userId
+  )
+  const showPrivateHabits = getFeatureFlagEnabledWithDefaultValue({
+    featureFlagId: FEATURE_FLAGS.VIEW_PRIVATE_HABITS_BY_DEFAULT,
+    flags: userFeatureFlags,
+    defaultValue: false
+  })
+
+  let beginDate: Date | undefined
+  let endDate: Date | undefined
+
+  if (tab === 'month') {
+    beginDate = startOfMonth(new Date())
+    endDate = endOfMonth(new Date())
+  }
+
+  if (tab === 'week') {
+    beginDate = startOfWeek(new Date())
+    endDate = endOfWeek(new Date())
+  }
 
   //get most the markedHabit within the current month
+
   const markedHabits = await prisma.markedHabit.findMany({
     select: {
       habitId: true
@@ -20,10 +56,11 @@ export const loader = async (args: LoaderFunctionArgs) => {
     where: {
       userId,
       date: {
-        gte: startOfCurrentMonth,
-        lte: endOfCurrentMonth
+        gte: beginDate,
+        lte: endDate
       },
       habit: {
+        private: showPrivateHabits ? undefined : false,
         deleted: false
       }
     },
@@ -48,6 +85,9 @@ export const loader = async (args: LoaderFunctionArgs) => {
     }
   })
 
+  //sort the markedHabit by amount desc
+  markedHabitCountByHabit.sort((a, b) => b.amount - a.amount)
+
   const habits = await prisma.habit.findMany({
     where: {
       id: {
@@ -56,38 +96,116 @@ export const loader = async (args: LoaderFunctionArgs) => {
     }
   })
 
-  return { markedHabits, markedHabitCountByHabit, habits }
+  const mostTrackedHabitAmount = markedHabitCountByHabit.reduce((a, b) =>
+    a.amount > b.amount ? a : b
+  )
+
+  const mostTrackedHabit = {
+    ...mostTrackedHabitAmount,
+    habit: habits.find((habit) => habit.id === mostTrackedHabitAmount.habitId)
+  }
+
+  return {
+    markedHabits,
+    markedHabitCountByHabit,
+    habits,
+    mostTrackedHabit,
+    tab
+  }
 }
 
 type LoaderData = typeof loader
 
 export default function Route() {
-  const { habits, markedHabitCountByHabit } = useLoaderData<LoaderData>()
+  const { habits, markedHabitCountByHabit, mostTrackedHabit, tab } =
+    useLoaderData<LoaderData>()
   return (
     <div className='mt-8 px-4 mx-auto md:max-w-4xl prose mb-8 lg:prose-xl'>
-      <h1 className=''>Trends</h1>
-      <p>
-        This page shows which habits have been marked the most for this month
-      </p>
-      {habits.map((h) => {
-        const amount =
-          markedHabitCountByHabit.find((habit) => habit.habitId === h.id)
-            ?.amount || 0
-        return (
-          <div className='grid grid-cols-2' key={h.id}>
-            <span>
-              {amount} <span className='text-xs'>days</span>
-            </span>
-
-            <span className='flex items-center gap-2'>
-              <div
-                className='w-6 h-6 rounded-full'
-                style={{ backgroundColor: h.colour }}></div>
-              {h.name}
+      <h1>Trends</h1>
+      <div role='tablist' className='not-prose tabs tabs-boxed'>
+        <Link
+          to={{
+            search: '?tab=week'
+          }}
+          role='tab'
+          className={`tab ${tab === 'week' ? 'tab-active' : ''}`}>
+          Week
+        </Link>
+        <Link
+          to={{
+            search: '?tab=month'
+          }}
+          role='tab'
+          className={`tab ${tab === 'month' ? 'tab-active' : ''}`}>
+          Month
+        </Link>
+        <Link
+          to={{
+            search: '?tab=all-time'
+          }}
+          role='tab'
+          className={`tab ${tab === 'all-time' ? 'tab-active' : ''}`}>
+          All Time
+        </Link>
+      </div>
+      <div className='flex flex-col lg:items-start lg:flex-row lg:gap-8 gap-4'>
+        <div className='card bg-base-200 mt-8 not-prose shadow-xl flex-1'>
+          <div className='card-body'>
+            <h2 className='card-title text-center'>Most Tracked</h2>
+            <div
+              style={{
+                borderColor: mostTrackedHabit.habit?.colour || 'transparent'
+              }}
+              className='h-40 w-40 border-[1rem] mx-auto my-4 rounded-full bg-transparent flex flex-col items-center justify-center'>
+              <span className='text-xl font-bold'>
+                {mostTrackedHabit.amount}
+              </span>
+              <span className='text-xs font-semibold'>times</span>
+            </div>
+            <span className='block text-center '>
+              {mostTrackedHabit.habit?.private ? (
+                <LockClosedIcon className='text-gray-400 w-4 h-4' />
+              ) : null}{' '}
+              {mostTrackedHabit.habit?.name}
             </span>
           </div>
-        )
-      })}
+        </div>
+        <div className='card bg-base-200 mt-8 not-prose shadow-xl flex-1'>
+          <div className='card-body'>
+            <div className='overflow-x-auto'>
+              <table className='table'>
+                {/* head */}
+                <thead>
+                  <tr>
+                    <th>Name</th>
+                    <th className='text-end'>Marked</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {markedHabitCountByHabit.map((mh) => {
+                    const habit = habits.find((h) => h.id === mh.habitId)
+                    const amount = mh.amount
+                    return (
+                      <tr key={mh.habitId}>
+                        <td className='flex items-center gap-2'>
+                          <div
+                            className='w-6 h-6 rounded-full'
+                            style={{ backgroundColor: habit?.colour }}></div>
+                          {habit?.private ? (
+                            <LockClosedIcon className='text-gray-400 w-4 h-4' />
+                          ) : null}{' '}
+                          {habit?.name}
+                        </td>
+                        <td className='text-end'>{amount}</td>
+                      </tr>
+                    )
+                  })}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
