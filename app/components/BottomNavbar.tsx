368eb115f78da5542f13e7f45b31aaa1e4c2f853
Sam Jonston <johnstonsam712@gmail.com>
2024-01-13 23:28:58 +0000

    skip rendering navbar if user isn't logged in

--- app/components/BottomNavbar.tsx
+++ app/components/BottomNavbar.tsx
@@ -1,54 +1,56 @@
-import React from 'react'
-import Trash from './icons/Trash'
-import { Link, NavLink, useRouteLoaderData } from '@remix-run/react'
-import Chevron from './icons/Chevron'
-import Home from './icons/Home'
-import CalendarDays from './icons/CalendarDays'
-import Squares from './icons/Squares'
-import UserCircle from './icons/UserCircle'
-import { RootLoaderData } from '~/root'
-import { ArrowTrendingUpIcon } from '@heroicons/react/24/outline'
+import React from "react";
+import Trash from "./icons/Trash";
+import { Link, NavLink, useRouteLoaderData } from "@remix-run/react";
+import Chevron from "./icons/Chevron";
+import Home from "./icons/Home";
+import CalendarDays from "./icons/CalendarDays";
+import Squares from "./icons/Squares";
+import UserCircle from "./icons/UserCircle";
+import { RootLoaderData } from "~/root";
+import { ArrowTrendingUpIcon } from "@heroicons/react/24/outline";
 
 export default function BottomNavbar() {
-  const rootLoaderData = useRouteLoaderData<RootLoaderData>('root')
+  const rootLoaderData = useRouteLoaderData<RootLoaderData>("root");
+  if (!rootLoaderData || !rootLoaderData.friends) return null;
   return (
-    <div className='btm-nav backdrop-blur-md bottom-nav bg-transparent'>
-      <NavItem name='Home' to='/' icon={<CalendarDays className='w-7 h-7' />} />
+    <div className="btm-nav backdrop-blur-md bottom-nav bg-transparent">
+      <NavItem name="Home" to="/" icon={<CalendarDays className="w-7 h-7" />} />
       <NavItem
-        name='Habits'
-        to='/habits'
-        icon={<Squares className='w-7 h-7' />}
+        name="Habits"
+        to="/habits"
+        icon={<Squares className="w-7 h-7" />}
       />
       <NavItem
-        name='Trends'
-        to='/trends'
-        icon={<ArrowTrendingUpIcon className='w-7 h-7' />}
+        name="Trends"
+        to="/trends"
+        icon={<ArrowTrendingUpIcon className="w-7 h-7" />}
       />
 
       <NavItem
-        name='Profile'
-        to='/profile'
-        icon={<UserCircle className='w-7 h-7' />}>
-        <div className='indicator'>
+        name="Profile"
+        to="/profile"
+        icon={<UserCircle className="w-7 h-7" />}
+      >
+        <div className="indicator">
           {rootLoaderData &&
           rootLoaderData.myReceivedFriendRequests &&
           rootLoaderData.myReceivedFriendRequests.length > 0 ? (
-            <span className='indicator-item badge badge-primary'>
+            <span className="indicator-item badge badge-primary">
               {rootLoaderData.myReceivedFriendRequests.length}
             </span>
           ) : null}
         </div>
       </NavItem>
     </div>
-  )
+  );
 }
 
 type NavItemProps = {
-  name: string
-  icon?: React.ReactNode
-  to: string
-  children?: React.ReactNode
-}
+  name: string;
+  icon?: React.ReactNode;
+  to: string;
+  children?: React.ReactNode;
+};
 
 function NavItem({ name, to, icon, children }: NavItemProps) {
   return (
@@ -56,20 +58,21 @@ function NavItem({ name, to, icon, children }: NavItemProps) {
       to={to}
       className={({ isActive, isPending }) =>
         `flex flex-col items-center justify-center px-2 text-gray-500 text-xs tracking-tighter gap-px transition bg-transparent ${
-          isActive ? 'active border-primary ' : ''
-        } ${isPending ? 'opacity-30' : ''}`
-      }>
+          isActive ? "active border-primary " : ""
+        } ${isPending ? "opacity-30" : ""}`
+      }
+    >
       {({ isActive, isPending }) => (
         <>
           {children}
-          <span className={`${isActive ? 'active text-primary' : ''}`}>
+          <span className={`${isActive ? "active text-primary" : ""}`}>
             {icon}
           </span>
-          <span className={`${isActive ? 'active text-primary' : ''}`}>
+          <span className={`${isActive ? "active text-primary" : ""}`}>
             {name}
           </span>
         </>
       )}
     </NavLink>
-  )
+  );
 }
