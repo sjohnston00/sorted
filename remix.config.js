de6cad80adb579581843745d8688975997fbf8de
Sam Jonston <johnstonsam712@gmail.com>
2024-01-14 22:06:22 +0000

    add remix crypto module

--- remix.config.js
+++ remix.config.js
@@ -7,6 +7,7 @@ module.exports = {
   // publicPath: "/build/",
   // serverDependenciesToBundle: 'all',
   serverDependenciesToBundle: [/@remix-pwa\/.*/],
+  browserNodeBuiltinsPolyfill: { modules: { crypto: true } },
   serverModuleFormat: "cjs",
   tailwind: true,
 };
