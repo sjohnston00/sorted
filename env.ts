393df91580e9a2719e0404bd659df1340ffbd323
Sam Jonston <johnstonsam712@gmail.com>
2024-04-26 15:30:33 +0100

    remove external cloud provider folders

--- env.ts
+++ env.ts
@@ -2,8 +2,6 @@ import { z } from "zod";
 
 const envVariables = z.object({
   DATABASE_URL: z.string(),
-  CLERK_PUBLISHABLE_KEY: z.string(),
-  CLERK_SECRET_KEY: z.string(),
   SESSION_SECRET: z.string(),
 });
 
